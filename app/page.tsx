6404e034d62fd62597f36d8367ddc3f392ae2881
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-03 17:20:52 +0300

    update 9 files and delete 19 files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex bg-[#f0f0f0] min-h-screen p-4 items-center justify-center">
+        <div className="bg-white p-5 flex flex-col rounded-md w-full">
+          <div className="grid w-full grid-cols-3 gap-4"></div>
+        </div>
+      </div>
+    </>
+  );
+}
